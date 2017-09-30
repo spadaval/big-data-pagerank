ae8cba7cdb3a0e5df6e7dc3c0bd73448a6a5e01e
Sumeet Padavala <sumeet.padavala@gmail.com>
2017-09-30 16:10:19 +0530

    Reorganize the project

--- matrixbuilder/PositionPairWritable.java
+++ matrixbuilder/PositionPairWritable.java
@@ -1,5 +1,31 @@
 package matrixbuilder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
+//Fancy stuff
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.io.GenericWritable;
+
 public class PositionPairWritable implements WritableComparable{
   // Some data
      private int i;
