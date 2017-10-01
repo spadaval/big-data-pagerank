bdb47ac1752401eab840bdc46dfd791575580eb3
Hadoop <sumeet.padavala@gmail.com>
2017-10-01 21:46:31 +0530

    Edit build script

---
+++ matrixbuilder/build.sh
@@ -0,0 +1,6 @@
+javac -classpath /usr/local/hadoop/share/hadoop/common/hadoop-common-2.7.2.jar:/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar:/usr/local/hadoop/share/hadoop/common/lib/commons-cli-1.2.jar *.java
+
+jar cvfe MatrixBuilder.jar *.class MatrixBuilder.class
+
+rm *.class
+
