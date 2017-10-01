fb12d16072251d79c189872b7e42de6b0af123a0
Sraman <sramakoo@gmail.com>
2017-10-02 01:56:20 +0530

    Delete build.sh

--- matrixmultiplier/build.sh
+++
@@ -1,9 +0,0 @@
-sudo javac -classpath /usr/local/hadoop/share/hadoop/common/hadoop-common-2.7.2.jar:/usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.2.jar:/usr/local/hadoop/share/hadoop/common/lib/commons-cli-1.2.jar:com.sun.tools.javac.Main -d /home/hduser/a0/ multiply.java
-
-sudo cp *.class class
-
-sudo jar -cvf multiply.jar -C /home/hduser/a0/class/ .
-
-cp class/*.jar .
-
-sudo rm -rf class
