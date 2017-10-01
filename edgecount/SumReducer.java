34c47356e68b7df33f8f1a3fec30847cc218c440
Sumeet Padavala <sumeet.padavala@gmail.com>
2017-10-01 23:19:43 +0530

    stuff

--- edgecount/SumReducer.java
+++ edgecount/SumReducer.java
@@ -15,9 +15,7 @@ public static class SumReducer
      extends Reducer<Text,LongWritable,Text,LongWritable> {
   private LongWritable result = new LongWritable();
 
-  public void reduce(Text key, Iterable<LongWritable> values,
-                     Context context
-                     ) throws IOException, InterruptedException {
+  public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
     int sum = 0;
     for (LongWritable val : values) {
       sum += val.get();
